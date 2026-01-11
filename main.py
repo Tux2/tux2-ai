03b106b5359129bfd1446abb8fe3cdd7f6b3165b
Tux2 <thetux2@gmail.com>
2026-01-11 15:38:46 -0700

    Verbose output options

--- main.py
+++ main.py
@@ -13,17 +13,22 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="Tux2 Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
+    v = args.verbose
+    if v:
+        print(f"User prompt: {args.user_prompt}")
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    
+
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=messages
     )
     if response.usage_metadata == None:
         raise RuntimeError("Unable to contact Gemini")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if v:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 if __name__ == "__main__":
